5decd9973619e2297575a502d38e721e265264fb
BLACK-NOIR09 <inambhat193201@gmail.com>
2024-09-15 16:04:19 +0530

    Project OOPS.cpp
    Date:-15/09/24

--- 9th.cpp
+++ 9th.cpp
@@ -9,30 +9,19 @@ private:
     int age;
 
 public:
-    // Constructor
     Animal(std::string n, int a) : name(n), age(a) {}
-
-    // Getter for name
     std::string getName() {
         return name;
     }
-
-    // Setter for name
     void setName(std::string n) {
         name = n;
     }
-
-    // Getter for age
     int getAge() {
         return age;
     }
-
-    // Setter for age
     void setAge(int a) {
         age = a;
     }
-
-    // Method to display animal details
     void display() {
         std::cout << "Name: " << name << ", Age: " << age << std::endl;
     }
@@ -44,20 +33,13 @@ private:
     std::string breed;
 
 public:
-    // Constructor
     Dog(std::string n, int a, std::string b) : Animal(n, a), breed(b) {}
-
-    // Getter for breed
     std::string getBreed() {
         return breed;
     }
-
-    // Setter for breed
     void setBreed(std::string b) {
         breed = b;
     }
-
-    // Overriding display method
     void display() {
         Animal::display();
         std::cout << "Breed: " << breed << std::endl;
@@ -67,10 +49,7 @@ public:
 // Polymorphism
 class Cat : public Animal {
 public:
-    // Constructor
     Cat(std::string n, int a) : Animal(n, a) {}
-
-    // Overriding display method
     void display() {
         std::cout << "This is a cat." << std::endl;
         Animal::display();
@@ -96,8 +75,6 @@ public:
         std::cout << "Meow Meow" << std::endl;
     }
 };
-
-// Main function
 int main() {
     Dog dog("SPY", 3, "LABRADOR");
     dog.display();
