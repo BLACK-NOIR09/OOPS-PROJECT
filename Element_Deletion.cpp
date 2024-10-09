dd1ba0142da55d9256d6fcdf51ce0dec9172d0d5
BLACK-NOIR09 <inambhat193201@gmail.com>
2024-10-09 14:24:42 +0530

    Update Element_Deletion.cpp

--- Element_Deletion.cpp
+++ Element_Deletion.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 
 void deleteElement(int arr[], int& n, int position) {
-    // Shift elements to the left to fill the gap created by the deleted element
     for (int i = position; i < n - 1; i++) {
         arr[i] = arr[i + 1];
     }
-    // Decrease the size of the array
     n--;
 }
 
 int main() {
     int arr[100] = {1, 2, 3, 4, 5}; // Initial array
-    int n = 5; // Current size of the array
+    int n = 5;
     int position;
 
     cout << "Original array: ";
@@ -20,12 +18,8 @@ int main() {
         cout << arr[i] << " ";
     }
     cout << endl;
-
-    // Get the position from the user
     cout << "Enter the position of the element to delete: ";
     cin >> position;
-
-    // Ensure the position is within the valid range
     if (position < 0 || position >= n) {
         cout << "Invalid position!" << endl;
         return 1;
