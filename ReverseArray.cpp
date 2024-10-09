3331bcdfd629da6be9c7960d828dd70765b03283
BLACK-NOIR09 <inambhat193201@gmail.com>
2024-10-09 14:30:12 +0530

    Update ReverseArray.cpp

--- ReverseArray.cpp
+++ ReverseArray.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
-
 void reverseArray(int arr[], int size) {
     int left = 0;
     int right = size - 1;
     
     while (left < right) {
-        // Swap the elements
         int temp = arr[left];
         arr[left] = arr[right];
         arr[right] = temp;
-        
-        // Move the pointers
         left++;
         right--;
     }
 }
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr[] = {5, 4, 3, 2, 1};
     int size = sizeof(arr) / sizeof(arr[0]);
 
     cout << "Original array: ";
